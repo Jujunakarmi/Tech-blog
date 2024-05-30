7121b215b10d7b0ba78f44da059276d602315c76
Juju Nakarmi <152660757+Jujunakarmi@users.noreply.github.com>
2024-05-30 16:59:27 -0400

    added dashboard route with get method that renders to dashboard homepage and dashboard handlebars

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,11 +1,33 @@
 const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
 
-
-
-router.get('/',(req, res) => {
+//will show post created by logged in user
+router.get('/',async (req, res) => {
   try {
-res.send('Dashboard')
+const postData = await Post.findAll({
+  where:{
+    user_id: req.session.user_id,
+  },
+  attributes: ['id', 'title', 'post_body', 'user_id'],
+
+  include:[
+    {model: Comment,
+      attributes: ['id', 'comment_body', 'user_id','post_id'],
+
+      include: {
+        model: User,
+      attributes:['username']
+      },
+    },
+  {
+    model:User,
+    attributes:['username']
+  }
+  ]
+})
   
+const posts = postData.map(((post) => post.get({plain:true})));
+res.render('dashboard', {posts, logged_in: true})
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
